397cb6fe90b92f80ae882da83d08770a9b78e19b
MF <milburnfernandes7@gmail.com>
2025-12-21 11:57:20 +0530

    Add 2D array examples and explanations in C

---
+++ arrays_2d.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int arraygame(int x,int y){
+    
+    int math[][10] = {{1,2,3,4,5},
+                     {6,7,8,9,10}};
+    printf("%d\n", math[x][y]);
+    
+}
+int main() {
+    //arraygame(1,1); example 
+    
+
+
+    return 0;
+}
---
+++ arrays_explained.c
@@ -0,0 +1,12 @@
+
+#include <stdio.h>
+//basic array
+int main() {
+int numbers[] = {1,2,3,4,5,6};
+char names[] = {'K','e','t','s','4','i'};
+for(int i = 0; i<6; i++){
+    printf("%d : %c\n",numbers[i],names[i]);
+}
+
+    return 0;
+}
